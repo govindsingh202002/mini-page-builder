1ad9e1ecc04ff6c131ceb708a2f1cec481658f32
Govind Singh <108587852+govindsingh202002@users.noreply.github.com>
2024-06-18 18:41:39 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import 'antd/dist/antd.css';
+import './App.css';
+import DragDropPage from './pages/dragdrop/dragdrop.pages';
+
+function App() {
+    return (
+        <div className='App'>
+            <DragDropPage />
+        </div>
+    );
+}
+
+export default App;
